3b22a1197b879d548f94028b99fa9be24ca2a518
Ricardo da Costa <rgdacosta@gmail.com>
2021-11-30 19:33:43 +0200

    Change greeting

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Greetings to my MA friends ... and the one in Canada! php version is " . PHP_VERSION . "\n";
+print Hello everyone in Canada! php version is " . PHP_VERSION . "\n";
 ?>
